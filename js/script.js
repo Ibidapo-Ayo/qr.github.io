8bc3f6236e5b3d6015cbf3d06b4889a01c279183
Ibidapo Ayomide Victor <ibidapoayomide754@gmail.com>
2022-12-31 15:16:54 +0100

    Share Btn Testing

--- js/script.js
+++ js/script.js
@@ -6,20 +6,24 @@ const GeneratedBtn = e => {
   clearUI(qr);
   const text = document.getElementById("text").value;
 
-  if ((text === "")) {
+  if (text === "") {
     alert("Please enter a text");
   } else {
     showSpinner();
     setTimeout(() => {
       hideSpinner();
       generateQr(text);
+     
       setTimeout(() => {
         const saveURL = qr.querySelector("img").src;
-        createSavebtn(saveURL,text);
+        createSavebtn(saveURL, text);
+        Share_Btn(saveURL)
       }, 50);
     }, 1000);
   }
 };
+
+
 
 const generateQr = text => {
   const qrcode = new QRCode("qrcode", {
@@ -28,7 +32,32 @@ const generateQr = text => {
     height: 200
   });
 };
-
+const Share_Btn = (saveURL) =>{
+    const output = document.getElementById("output")
+    document.getElementById("share_btn").addEventListener('click',async ()=>{
+    
+      console.log(saveURL)
+      if (!navigator.canShare) {
+          output.textContent = `Your browser doesn't support the Web Share API.`
+          return
+        }
+      
+        if (navigator.canShare({ saveURL })) {
+          try {
+            await navigator.share({
+              saveURL,
+              title: 'Images',
+              text: 'Beautiful images'
+            })
+            output.textContent = 'Shared!'
+          } catch (error) {
+            output.textContent = `Error: ${error.message}`
+          }
+        } else {
+          output.textContent = `Your system doesn't support sharing these files.`
+        }
+    })
+  }
 const createSavebtn = (saveURL, text) => {
   var downloadLink = document.createElement("a");
   downloadLink.id = "download-link";
@@ -60,7 +89,5 @@ hideSpinner();
 form.addEventListener("submit", GeneratedBtn);
 
 
-// Form 
 
-
-
+// Form
